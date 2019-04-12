016b282e6f22677bcb4fafbfbb90886c503fc3db
hwmaltby <hwmaltby@berkeley.edu>
2019-04-11 23:41:13 -0700

    Completed Set 1 Challenge 2

---
+++ set_1/fixed_xor.py
@@ -0,0 +1,7 @@
+def hex_xor(s1, s2):
+	"""
+	Returns the hexadecimal output of s1 xor s2.
+	"""
+	return hex(int(s1, 16) ^ int(s2, 16))[2:]
+
+assert(hex_xor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965") == "746865206b696420646f6e277420706c6179")
